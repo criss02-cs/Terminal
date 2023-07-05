8a6523306aee84b6f0c9c9824fdb7537aaf777cc
gbdev <cristianoaloigi0@gmail.com>
2023-07-05 17:43:35 +0200

    Initial Commit

---
+++ Terminal/TerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalApp.swift
+//  Terminal
+//
+//  Created by GBdev on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
